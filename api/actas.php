ddfd73162e91ed8842d9cb7e90594be3033be4e0
Iván Rodriguez <ivanrodriguez.soft@gmail.com>
2019-08-28 13:20:17 -0300

    Actualización en archivo

--- api/actas.php
+++ api/actas.php
@@ -4,8 +4,7 @@ if ($method == "GET") {
         $data = DB::query("SELECT * FROM $tableName WHERE id=:id", array(":id" => $id));
         if($data != null) {
             echo json_encode($data[0], JSON_PRETTY_PRINT);
-        } else {
-            //header("HTTP/1.0 404 Not Found"); //Retorna resultado de recurso no encontrado. (Regla de "Estado HTTP", de una API REST)
+        } else {            
             echo json_encode(["message" => "Acta inexistente", "response" => "Error."],  JSON_PRETTY_PRINT);
         }            
     } else {
@@ -13,14 +12,13 @@ if ($method == "GET") {
         echo json_encode($data, JSON_PRETTY_PRINT);        
     }
 } elseif ($method == "POST") {
-    if ($_POST != null || 2==2) { //Mejorar esta condicion y esta parte del codigo.                       
+    if ($_POST != null || 2==2) {                  
         extract(json_decode(file_get_contents("php://input"), true));
         DB::query("INSERT INTO $tableName VALUES (null, :numero, :titulo, :fecha, :estado)", array(":numero" => $numero, ":titulo" => $titulo, ":fecha" => $fecha, ":estado" => $estado));        
         $data = DB::query("SELECT * FROM $tableName ORDER BY id DESC LIMIT 1");       
         echo json_encode(["message" => "Acta guardada correctamente.", "response" => "Exito.", "newActa" => $data[0]], JSON_PRETTY_PRINT);
     } else {
-        echo json_encode(['message' => "No se puede crear una nueva Acta.", "response" => "Error."]);        
-        //header("HTTP/1.1 400 Bad Request"); //Resultado con cabecera HTTP de peticion incorrecta.
+        echo json_encode(['message' => "No se puede crear una nueva Acta.", "response" => "Error."]);               
     }    
 } elseif ($id) {
     $data = DB::query("SELECT * FROM $tableName WHERE id=:id", array(":id" => $id));
